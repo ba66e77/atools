fe1415cb0190f85821bbf1616e262016a165b0cc
Barrett Smith <barrett.smith@acquia.com>
2014-08-30 21:57:32 -0400

    add file name input and aggregation

--- loganalysis/mysqlSlowQueryLogCounter.sh
+++ loganalysis/mysqlSlowQueryLogCounter.sh
@@ -1,1 +1,5 @@
-grep 'INSERT\|UPDATE\|SELECT\|DELETE' testFile.txt | sed 's/\(.*\)VALUES.*/\1/' | sed 's/\(.*\)SET.*/\1/' | sed 's/\(.*\)JOIN.*/\1/'
+#!/usr/local/bin/bash
+
+logFile=$1
+
+grep 'INSERT\|UPDATE\|SELECT\|DELETE' $logFile | sed 's/\(.*\)VALUES.*/\1/' | sed 's/\(.*\)SET.*/\1/' | sed 's/\(.*\)JOIN.*/\1/' | sed 's/\(.*\)WHERE.*/\1/' | sort | uniq -c | sort -rn
